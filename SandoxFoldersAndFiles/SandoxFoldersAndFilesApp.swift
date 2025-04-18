4338797b02e72075f9e01463e8e81d69e95d1523
Javier Calatrava <javi.calatrava@gmail.com>
2025-04-18 11:23:49 +0200

    Blank project

---
+++ SandoxFoldersAndFiles/SandoxFoldersAndFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandoxFoldersAndFilesApp.swift
+//  SandoxFoldersAndFiles
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 18/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SandoxFoldersAndFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
